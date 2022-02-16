78d833939d16425b6520de28bd06ab1688a13644
luckyhyom <bs_khm@naver.com>
2022-02-17 00:46:28 +0900

    로컬에서 한개의 이미지로 여러개의 컨테이너를 띄워보았다.

--- local/index.js
+++ local/index.js
@@ -3,9 +3,14 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('this is from docker container.')
+  res.send('this is from docker container. 나는 연봉 오백억. 알겠냐! 너무 좋구요')
 });
 
 app.listen(port, () => {
   console.log('good!');
 })
+/**
+ * 컨테이너는 자신의 소스코드를 보고있기 때문에 로컬에서 수정해도 알수가 없다.
+ * nodemon을 정상작동하게 하고 싶다면, 컨테이너가 로컬의 소스코드를 보게끔 하여 수정됨을 인지할수있게 한다.
+ * https://www.daleseo.com/docker-volumes-bind-mounts/
+ */
